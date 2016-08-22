3713783346c6fa3f25a9269798ae756f5f9eb037
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-08-22 12:46:48 +0200

    Add public headers

---
+++ Sources/EmojiTimeFormatter.h
@@ -0,0 +1,18 @@
+//
+//  EmojiTimeFormatter.h
+//  EmojiTimeFormatter
+//
+//  Created by Thomas Paul Mann on 22/08/16.
+//  Copyright © 2016 Thomas Paul Mann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Version.
+FOUNDATION_EXPORT double VersionVersionNumber;
+
+//! Project version string for Version.
+FOUNDATION_EXPORT const unsigned char VersionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Version/PublicHeader.h>
+
