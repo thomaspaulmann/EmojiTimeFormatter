3074080ea6e1683940fae27ab000180b37cd7211
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-08-22 11:38:26 +0200

    Correct time parsing

--- Sources/EmojiTimeFormatter.swift
+++ Sources/EmojiTimeFormatter.swift
@@ -37,62 +37,127 @@ public class EmojiTimeFormatter: Formatter {
     // MARK: - Private
 
     private func string(for time: Double) -> String {
-        // Limit time to 12 hours.
-        let limitedTime = time > 12 ? time - 12 : time
+        let limitedTime = limit(time: time)
 
-        if limitedTime < 0.25 {
+        if limitedTime < 1.25 {
             return ClockFaceEmoji.twelve.rawValue
-        } else if limitedTime < 0.75 {
-            return ClockFaceEmoji.twelveThirty.rawValue
-        } else if limitedTime < 1.25 {
-            return ClockFaceEmoji.one.rawValue
         } else if limitedTime < 1.75 {
-            return ClockFaceEmoji.oneThirty.rawValue
+            return ClockFaceEmoji.twelveThirty.rawValue
         } else if limitedTime < 2.25 {
-            return ClockFaceEmoji.two.rawValue
+            return ClockFaceEmoji.one.rawValue
         } else if limitedTime < 2.75 {
-            return ClockFaceEmoji.twoThirty.rawValue
+            return ClockFaceEmoji.oneThirty.rawValue
         } else if limitedTime < 3.25 {
-            return ClockFaceEmoji.three.rawValue
+            return ClockFaceEmoji.two.rawValue
         } else if limitedTime < 3.75 {
-            return ClockFaceEmoji.threeThirty.rawValue
+            return ClockFaceEmoji.twoThirty.rawValue
         } else if limitedTime < 4.25 {
-            return ClockFaceEmoji.four.rawValue
+            return ClockFaceEmoji.three.rawValue
         } else if limitedTime < 4.75 {
-            return ClockFaceEmoji.fourThirty.rawValue
+            return ClockFaceEmoji.threeThirty.rawValue
         } else if limitedTime < 5.25 {
-            return ClockFaceEmoji.five.rawValue
+            return ClockFaceEmoji.four.rawValue
         } else if limitedTime < 5.75 {
-            return ClockFaceEmoji.fiveThirty.rawValue
+            return ClockFaceEmoji.fourThirty.rawValue
         } else if limitedTime < 6.25 {
-            return ClockFaceEmoji.six.rawValue
+            return ClockFaceEmoji.five.rawValue
         } else if limitedTime < 6.75 {
-            return ClockFaceEmoji.sixThirty.rawValue
+            return ClockFaceEmoji.fiveThirty.rawValue
         } else if limitedTime < 7.25 {
-            return ClockFaceEmoji.seven.rawValue
+            return ClockFaceEmoji.six.rawValue
         } else if limitedTime < 7.75 {
-            return ClockFaceEmoji.sevenThirty.rawValue
+            return ClockFaceEmoji.sixThirty.rawValue
         } else if limitedTime < 8.25 {
-            return ClockFaceEmoji.eight.rawValue
+            return ClockFaceEmoji.seven.rawValue
         } else if limitedTime < 8.75 {
-            return ClockFaceEmoji.eightThirty.rawValue
+            return ClockFaceEmoji.sevenThirty.rawValue
         } else if limitedTime < 9.25 {
-            return ClockFaceEmoji.nine.rawValue
+            return ClockFaceEmoji.eight.rawValue
         } else if limitedTime < 9.75 {
-            return ClockFaceEmoji.nineThirty.rawValue
+            return ClockFaceEmoji.eightThirty.rawValue
         } else if limitedTime < 10.25 {
-            return ClockFaceEmoji.ten.rawValue
+            return ClockFaceEmoji.nine.rawValue
         } else if limitedTime < 10.75 {
-            return ClockFaceEmoji.tenThirty.rawValue
+            return ClockFaceEmoji.nineThirty.rawValue
         } else if limitedTime < 11.25 {
-            return ClockFaceEmoji.eleven.rawValue
+            return ClockFaceEmoji.ten.rawValue
         } else if limitedTime < 11.75 {
-            return ClockFaceEmoji.elevenThirty.rawValue
+            return ClockFaceEmoji.tenThirty.rawValue
         } else if limitedTime < 12.25 {
+            return ClockFaceEmoji.eleven.rawValue
+        } else if limitedTime < 12.75 {
+            return ClockFaceEmoji.elevenThirty.rawValue
+        } else  {
             return ClockFaceEmoji.twelve.rawValue
         }
+    }
 
-        return ""
+    private func date(for emoji: ClockFaceEmoji) -> Date {
+        let halfHour = TimeInterval(1800) // in seconds
+
+        switch emoji {
+        case .twelve:
+            return Date(timeIntervalSince1970: 0)
+        case .twelveThirty:
+            return Date(timeIntervalSince1970: 1 * halfHour)
+        case .one:
+            return Date(timeIntervalSince1970: 2 * halfHour)
+        case .oneThirty:
+            return Date(timeIntervalSince1970: 3 * halfHour)
+        case .two:
+            return Date(timeIntervalSince1970: 4 * halfHour)
+        case .twoThirty:
+            return Date(timeIntervalSince1970: 5 * halfHour)
+        case .three:
+            return Date(timeIntervalSince1970: 6 * halfHour)
+        case .threeThirty:
+            return Date(timeIntervalSince1970: 7 * halfHour)
+        case .four:
+            return Date(timeIntervalSince1970: 8 * halfHour)
+        case .fourThirty:
+            return Date(timeIntervalSince1970: 9 * halfHour)
+        case .five:
+            return Date(timeIntervalSince1970: 10 * halfHour)
+        case .fiveThirty:
+            return Date(timeIntervalSince1970: 11 * halfHour)
+        case .six:
+            return Date(timeIntervalSince1970: 12 * halfHour)
+        case .sixThirty:
+            return Date(timeIntervalSince1970: 13 * halfHour)
+        case .seven:
+            return Date(timeIntervalSince1970: 14 * halfHour)
+        case .sevenThirty:
+            return Date(timeIntervalSince1970: 15 * halfHour)
+        case .eight:
+            return Date(timeIntervalSince1970: 16 * halfHour)
+        case .eightThirty:
+            return Date(timeIntervalSince1970: 17 * halfHour)
+        case .nine:
+            return Date(timeIntervalSince1970: 18 * halfHour)
+        case .nineThirty:
+            return Date(timeIntervalSince1970: 19 * halfHour)
+        case .ten:
+            return Date(timeIntervalSince1970: 20 * halfHour)
+        case .tenThirty:
+            return Date(timeIntervalSince1970: 21 * halfHour)
+        case .eleven:
+            return Date(timeIntervalSince1970: 22 * halfHour)
+        case .elevenThirty:
+            return Date(timeIntervalSince1970: 23 * halfHour)
+        }
+    }
+
+    private func limit(time: Double) -> Double {
+        if time < 1 {
+            // Midnight
+            return time + 12
+        } else if time > 13 {
+            // Afternoon
+            return time - 12
+        } else {
+            // Rest of the day
+            return time
+        }
     }
 
 }
